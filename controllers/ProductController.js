b845e101bced43448c64a234b2b4221f3798eb83
abellaali <achraf.bellaali@a-sis.com>
2019-05-03 18:00:13 +0100

    First commit in version 2 branch

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,19 +1,20 @@
-appModule.controller('ProductController', function($scope, $window, $routeParams, $rootScope, $route) {
+/*
+    https://stackoverflow.com/questions/35091524/spring-cors-no-access-control-allow-origin-header-is-present
+
+    There is a problem to resolve later NO-ACCESS-CONTROL-ALLOW-ORIGIN
+*/
+appModule.controller('ProductController', function($scope, $window, $routeParams, $rootScope, $route, $http) {
     $scope.products = [];
-    var db = firebase.firestore();
 
     function init() {
-        //////////////////////////////////////////////////////////////////////
-        // https://firebase.google.com/docs/firestore/query-data/get-data  //
-        //////////////////////////////////////////////////////////////////////
-        // Read data from collection "products"
-        db.collection("products").get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
+        $http.get('http://localhost:8080/products/').then(function(rep){
+            console.log(rep);
+            rep.forEach(element => {
                 $scope.products.push({
-                    id: doc.id,
-                    designation: doc.data().designation,
-                    unitPrice: doc.data().unitPrice,
-                    availableQuantity: doc.data().availableQuantity,
+                    id: element.id,
+                    designation: element.designation,
+                    unitPrice: element.unitPrice,
+                    availableQuantity: element.availableQuantity
                 });
             });
             $scope.$apply();
@@ -28,56 +29,20 @@ appModule.controller('ProductController', function($scope, $window, $routeParams
             availableQuantity: product.availableQuantity
         };
         
-        //////////////////////////////////////////////////////////////////////
-        // https://firebase.google.com/docs/firestore/manage-data/add-data  //
-        //////////////////////////////////////////////////////////////////////
-        // Add a new document in collection "products"
-        // the function makeid(integer) is declared in assets/js/helpers.js
-        db.collection("products").doc(makeid(15))
-        .set($scope.product)
-        .then(function() {
-            console.log("Document successfully written!");
-            $window.location.href = '/products';
-        })
-        .catch(function(error) {
-            console.error("Error writing document: ", error);
-        });
+        //POST Request to http://localhost:8080/products/
     };
 
     $scope.updateProductInit = function() {
         var id = $routeParams.id;
-        console.log('id init : ', id);
-        query = db.collection("products").doc(id).get()
-        .then(function(doc) {
-            if(doc.exists){
-                $scope.product = {
-                    designation: doc.data().designation,
-                    unitPrice: doc.data().unitPrice,
-                    availableQuantity: doc.data().availableQuantity
-                };
-                $scope.$apply();
-            }
-            else{
-                console.log("No such document !");
-            }
-        })
-        .catch(function(error){
-            console.log("Error getting document: ", error);
-        });
+        
+        //Get Request to get one product by id
+        // http://localhost:8080/products/{id}
     };
 
     $scope.updateProduct = function(product) {
         var id = $routeParams.id;
 
-        db.collection("products").doc(id)
-        .set($scope.product)
-        .then(function() {
-            console.log("Document successfully written!");
-            $window.location.href = '/products';
-        })
-        .catch(function(error) {
-            console.error("Error writing document: ", error);
-        });
+        //PUT request http://localhost:8080/products/{id}
     };
 
     $scope.removeModal = function(id){
@@ -87,22 +52,7 @@ appModule.controller('ProductController', function($scope, $window, $routeParams
     $scope.removeProduct = function(id) {
         var id = $rootScope.selectedId;
         
-        db.collection("products").doc(id).delete()
-        .then(function() {
-            $scope.delete = {
-                success: true,
-                fail: false
-            };
-            $scope.$apply();
-        })
-        .catch(function(error) {
-            console.error("Error removing document: ", error);
-            $scope.delete = {
-                success: false,
-                fail: true
-            };
-            $scope.$apply();
-        });
+        //DELETE request http://localhost:8080/products/{id}
     };
 
     $scope.reload = function(){
